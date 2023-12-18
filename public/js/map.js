52e842d6fbc878f8b6d0560cf8fbcbefc919780d
Chitvan Gupta <chitvangupta01@gmail.com>
2023-12-18 21:17:42 +0530

    Intial Commit

---
+++ public/js/map.js
@@ -0,0 +1,19 @@
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  // style: "mapbox://styles/mapbox/light-v11",
+  center: listing.geometry.coordinates, // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
+
+// console.log(coordinates);
+
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates) // Listing.geometry.coordinates
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`
+    )
+  )
+  .addTo(map);
---
+++ routes/listing.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+
+const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index)) //* Index Route
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing) //* Create Route
+  );
+
+//* New Route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing)) //* Show Route
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.updateListing) //* Update Route
+  )
+  .delete(
+    isLoggedIn,
+    isOwner,
+    wrapAsync(listingController.deleteListing) //* Delete Route
+  );
+
+//* Edit Route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.editListing)
+);
+
+module.exports = router;
